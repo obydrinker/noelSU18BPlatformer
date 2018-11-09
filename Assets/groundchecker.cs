f7521e7f121e1998fce2473f5685be959c7f48ac
Noel Mortensen <noel.mortensen@elev.ga.lbs.se>
2018-11-09 16:00:06 +0100

    map and movement
    la till en liten fin bana och klas lärde sig att gå och hoppa lite

---
+++ Assets/groundchecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundchecker : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+
+}
